90ad712aa7eba7e93081012ac827195d6d701a28
mabgcm <bugucam@gmail.com>
2023-02-04 19:38:46 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import Home from './Pages/Home';
+import Navbar from './Components/Navbar';
+import Contact from './Pages/Contact';
+import Portfolio from './Pages/Portfolio';
+import Resume from './Pages/Resume';
+import Blog from "./Pages/Blog";
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='Resume' element={<Resume />} />
+        <Route path='Portfolio' element={<Portfolio />} />
+        <Route path='Contact' element={<Contact />} />
+        <Route path='Blog' element={<Blog />} />
+      </Routes>
+    </div>
   );
 }
 
