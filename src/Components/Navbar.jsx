3c5e5f189e40cde5c62f1a0d7253d27ce11d181e
mabgcm <bugucam@gmail.com>
2023-02-04 20:28:11 -0400

    namehover

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -69,7 +69,7 @@ function Navbar() {
                         Matt Ali Bugucam
                     </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, }}>
                         <IconButton
                             size="large"
                             aria-label="account of current user"
@@ -122,6 +122,9 @@ function Navbar() {
                             letterSpacing: '.3rem',
                             color: 'inherit',
                             textDecoration: 'none',
+                            '&:hover': {
+                                color: 'inherit'
+                            }
                         }}
                     >
                         Matt Ali Bugucam
