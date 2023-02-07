6831b9217294181dc0c1e6e83d84f71ec2055b8e
mabgcm <bugucam@gmail.com>
2023-02-07 17:21:20 -0400

    last attempt

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -4,6 +4,7 @@ import TextField from '@mui/material/TextField';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import emailjs from '@emailjs/browser';
+import { Paper } from '@mui/material';
 
 
 const Contact = () => {
@@ -29,44 +30,46 @@ const Contact = () => {
 
     return (
         <section className='m-5 pt-5 text-center'>
-            <h1 className=''>Contact Form</h1>
-            <Box
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '70vw' },
-                }}
-                noValidate
-                autoComplete="off"
-                ref={form} onSubmit={sendEmail}
-            >
-                <div>
-                    <TextField
-                        required
-                        id="user_name"
-                        name="from_name"
-                        label="Your Name"
-                        placeholder="Enter your name here.."
-                    />
-                    <TextField
-                        required
-                        id="email"
-                        name="user_email"
-                        label="Your E-mail"
-                        type="email"
-                        placeholder="Enter your email here.."
-                    />
-                    <TextField
-                        id="message"
-                        name="message"
-                        label="Your Message"
-                        multiline
-                        rows={4}
-                        placeholder="Default Value"
-                    />
-                </div>
-                <button id='signin' onClick={notify} variant="success" type="submit" className='btn btn-success w-25'>Send</button>
-                <ToastContainer />
-            </Box>
+            <Paper className='paper'>
+                <h1 className=''>Contact Form</h1>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '70vw' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    ref={form} onSubmit={sendEmail}
+                >
+                    <div>
+                        <TextField
+                            required
+                            id="user_name"
+                            name="from_name"
+                            label="Your Name"
+                            placeholder="Enter your name here.."
+                        />
+                        <TextField
+                            required
+                            id="email"
+                            name="user_email"
+                            label="Your E-mail"
+                            type="email"
+                            placeholder="Enter your email here.."
+                        />
+                        <TextField
+                            id="message"
+                            name="message"
+                            label="Your Message"
+                            multiline
+                            rows={4}
+                            placeholder="Default Value"
+                        />
+                    </div>
+                    <button id='signin' onClick={notify} variant="success" type="submit" className='btn btn-success w-25'>Send</button>
+                    <ToastContainer />
+                </Box>
+            </Paper>
         </section>
     )
 }
