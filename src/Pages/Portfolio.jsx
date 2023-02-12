8bd37a29aba8dee0317cdae60f70c25a26f84220
mabgcm <bugucam@gmail.com>
2023-02-12 01:02:11 -0400

    modal styling complete

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -22,16 +22,18 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: {
-        xs: 400,
-        sm: 600,
+        xs: 350,
+        sm: 350,
         md: 600,
         lg: 600,
         xl: 600,
     },
+    height: 500,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
+    overflow: 'scroll'
 };
 
 
