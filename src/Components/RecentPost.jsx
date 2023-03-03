d9e544156a34d23c408ae3d91c270f0958aa1ffe
mabgcm <bugucam@gmail.com>
2023-03-02 23:06:56 -0400

    featured post

--- src/Components/RecentPost.jsx
+++ src/Components/RecentPost.jsx
@@ -25,13 +25,15 @@ const RecentPost = () => {
 
     return (
         <div className="col-lg-4">
-            {blogs.slice(0, 1).map(({ id, title, category, likes, parag1, profile, image1, author, timestamp }) => (
+            {blogs.slice(0, 1).map(({ id, title, category, likes, parag1, parag2, parag3, profile, image1, author, timestamp }) => (
                 <div className="post-entry-1 lg" key={id}>
                     <Link to={`/blogpost/${id}`}><img src={image1} alt="" className="img-fluid" /></Link>
                     <div className="post-meta"><span className="date">{category}</span> <span className="mx-1"><CircleIcon fontSize='xsmall' /></span> <span>{new Date(timestamp?.toDate()).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: '2-digit' }).toUpperCase()}</span>
                     </div>
                     <h2><Link to={`/blogpost/${id}`}>{title}</Link></h2>
                     <p className="mb-4 d-block">{parag1}</p>
+                    <p className="mb-4 d-block">{parag2}</p>
+                    <p className="mb-4 d-block">{parag3}</p>
 
                     <div className="d-flex align-items-center author">
                         <div className="photo"><img src={profile} alt="" className="img-fluid" /></div>
